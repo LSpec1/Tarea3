99a3fea3c6475029a0862ab73c4509e2728bff7f
Ignacio <kjulo.c@gmail.com>
2021-10-16 21:23:07 -0300

    Navbar completado
    Navbar ya redirige a las distintas paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactScreenComponent } from './screens/contact-screen/contact-screen.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { NewsScreenComponent } from './screens/news-screen/news-screen.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeScreenComponent},
+  { path: 'contact', component: ContactScreenComponent},
+  { path: 'news', component: NewsScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
